f0fbc7ef582762e163ecd7488f3cf51ab5c334b4
charleytrigano <charley@trigano.org>
2026-01-18 09:24:23 +0100

    Update config.py

--- config.py
+++ config.py
@@ -1,1 +1,10 @@
+COMPTE_TO_POSTE = {
+    "Entretien plomberie": "Entretien – Plomberie",
+    "Entretien serrurerie": "Entretien – Serrurerie",
+    "Contrat entretien ascenseur": "Contrat – Ascenseur",
+    "Electricite Ascenseur 35%": "Énergie – Ascenseur",
+    "Eau": "Eau",
+    "Assurances": "Assurances",
+    "Honoraires Syndic": "Honoraires – Syndic",
+}
 
