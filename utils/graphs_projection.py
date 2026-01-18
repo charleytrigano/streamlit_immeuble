2503ee5e5d00ec9e0f7d356a9c8c9dda3247277b
charleytrigano <charley@trigano.org>
2026-01-18 09:51:12 +0100

    Update graphs_projection.py

--- utils/graphs_projection.py
+++ utils/graphs_projection.py
@@ -1,1 +1,14 @@
+import streamlit as st
+
+def plot_projection(df):
+    st.subheader("Projection des charges")
+
+    chart = (
+        df.groupby(["Année", "Scénario"])["Montant_Projeté"]
+        .sum()
+        .reset_index()
+        .pivot(index="Année", columns="Scénario", values="Montant_Projeté")
+    )
+
+    st.line_chart(chart)
 
