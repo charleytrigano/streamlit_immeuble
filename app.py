c63932c34bde2cad8b6290f6b0fe1f0dcc651263
charleytrigano <charley@trigano.org>
2026-01-27 15:36:30 +0100

    Update app.py

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ from datetime import date
 st.set_page_config(page_title="Pilotage immeuble", layout="wide")
 
 SUPABASE_URL = st.secrets["SUPABASE_URL"]
-SUPABASE_KEY = st.secrets["SUPABASE_ANON_KEY"]
+SUPABASE_KEY = st.secrets["SUPABASE_KEY"]
 supabase = create_client(SUPABASE_URL, SUPABASE_KEY)
 
 # =====================
