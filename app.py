11d51d5b01c3f155779a1b894f0e32005beaa6e2
charleytrigano <charley@trigano.org>
2026-01-28 09:33:37 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,36 @@
 import streamlit as st
+from supabase import create_client
 from utils.depenses_ui import depenses_ui
 
+# -----------------------------
+# CONFIG STREAMLIT
+# -----------------------------
+st.set_page_config(
+    page_title="Immeuble – Pilotage",
+    layout="wide"
+)
+
+# -----------------------------
+# SUPABASE
+# -----------------------------
+SUPABASE_URL = st.secrets["SUPABASE_URL"]
+SUPABASE_ANON_KEY = st.secrets["SUPABASE_ANON_KEY"]
+
+supabase = create_client(SUPABASE_URL, SUPABASE_ANON_KEY)
+
+# -----------------------------
+# MAIN
+# -----------------------------
 def main():
     st.sidebar.title("Navigation")
-    depenses_ui(None)
+    page = st.sidebar.radio(
+        "Menu",
+        ["Dépenses"]
+    )
+
+    if page == "Dépenses":
+        depenses_ui(supabase)
+
 
 if __name__ == "__main__":
     main()
