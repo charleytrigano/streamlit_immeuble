8b4a594b37630b1518c78040f64a7ace6fbfcadb
charleytrigano <charley@trigano.org>
2026-01-28 16:46:29 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,11 @@
 import streamlit as st
 from supabase import create_client
 
+
+
 from utils.budget_vs_reel_ui import budget_vs_reel_ui
 budget_vs_reel_ui(supabase, annee)
+
 
 # =========================
 # CONFIG STREAMLIT
