8bfe56425dc4d9d1ab2eea7edbea06c9d95d57e2
charleytrigano <charley@trigano.org>
2026-01-31 21:14:08 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,16 @@
+
+
+
 import os
+import sys
 import streamlit as st
+
+st.write("📂 CWD =", os.getcwd())
+st.write("📁 FICHIERS =", os.listdir("."))
+st.write("🐍 sys.path =", sys.path)
+
+
+
 
 st.write("📁 Contenu du dossier courant :")
 st.write(os.listdir("."))
