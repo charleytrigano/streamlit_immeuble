3f62618244edc04f7ea119196ca36454a47bf5f0
charleytrigano <charley@trigano.org>
2026-01-31 11:02:36 +0100

    Update supabase_client.py

--- supabase_client.py
+++ supabase_client.py
@@ -1,18 +1,8 @@
-import streamlit as st
 from supabase import create_client
+import streamlit as st
 
 
 def get_supabase_client():
-    try:
-        url = st.secrets["SUPABASE_URL"]
-        key = st.secrets["SUPABASE_ANON_KEY"]
-    except KeyError:
-        st.error(
-            "❌ Supabase mal configuré.\n\n"
-            "Ajoute dans secrets.toml :\n"
-            "SUPABASE_URL\n"
-            "SUPABASE_ANON_KEY"
-        )
-        st.stop()
-
+    url = st.secrets["SUPABASE_URL"]
+    key = st.secrets["SUPABASE_ANON_KEY"]
     return create_client(url, key)
